bd92bf969b3f202425823a73b5e0229f84f49106
yuxingfirst <yuxingfirst@gmail.com>
2014-01-06 10:11:37 +0800

    修改README.md;增加Licence

--- src/cs_log.c
+++ src/cs_log.c
@@ -1,8 +1,36 @@
 /*
- * this log componet get from twemproxy and do some modify
- * twemproxy project home: https://github.com/twitter/twemproxy
- */
+ * coro_sched - A mini coroutine schedule framework
+ * Copyright (C) 2014 yuxingfirst@gmail.com.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
+/*
+ * twemproxy - A fast and lightweight proxy for memcached protocol.
+ * Copyright (C) 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "cs_common.h"
 
 static struct logger logger;
